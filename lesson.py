23824d06b4744ae71f370cb47964541e6d0c05ec
JericoDev <99786648+JericoDev@users.noreply.github.com>
2024-09-23 07:32:51 +0800

    Add files via upload

---
+++ lesson.py
@@ -0,0 +1,63 @@
+# i = 5
+# while i > 0:
+#     print(i)
+#     i -= 1
+
+# ===========================================
+
+# i = 0
+# sum = 0
+# # 0 1 2 3 4
+# while i <= 5:
+#     sum += i
+#     i += 1
+# print(sum)
+
+# ===========================================
+
+# num_input = int(input("Enter first number: "))
+# sum = 0
+# while num_input != 0:
+#     sum += num_input
+#     num_input = int(input("Enter another number, make it 0 to total all: "))
+# print(sum)
+
+# ===========================================
+
+# total = 0
+# i = 0
+# # 0 1 2 3 4
+# while i <= 6:
+#     if i % 2 == 0:
+#         total += i
+#     i += 1
+# print(total)
+
+# ===========================================
+
+# word = "hello"
+# input_guess = input("Enter a word: ")
+# while input_guess != word:
+#     input_guess = input("Enter a word: ")
+# print("You are correct!")
+
+
+# ===========================================
+
+# import random
+
+# while True:
+#     number = random.randint(1, 5)
+    
+#     print(number)
+    
+#     if number == 3:
+#         break
+
+# print("The loop has ended because the number 3 was generated.")
+
+
+
+
+
+
